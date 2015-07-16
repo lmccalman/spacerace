248e861464f28555e4f6b3c3267624318b4d8311
Brian Thorne <brian.thorne@nicta.com.au>
2015-07-16 20:56:54 +1000

    Use socket.io to send messages to/from the browser.

---
+++ frontend/README.md
@@ -0,0 +1,18 @@
+# Spacerace UI
+
+## Frontend Options
+
+- angular 1
+- D3
+- angular 2
+- canvas
+
+# Running
+ 
+For now in one terminal run the dummy producer:
+
+    $ node producer.js
+
+Then start the webserver:
+
+    $ node websocket-server.js
--- frontend/websocket-server.js
+++ frontend/websocket-server.js
@@ -1,13 +1,40 @@
 var zmq = require('zmq');
+var app = require('express')();
+var http = require('http').Server(app);
 
+var io = require('socket.io')(http);
+
+// Webserver component
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+http.listen(8000, function(){
+  console.log('listening on *:8000');
+});
+
+// Websocket component
+io.on('connection', function(ws){
+    console.log('A websocket connected');
+
+    ws.on('debug', function(msg){
+        console.log('WWW Debug: ' + msg);
+    });
+
+    ws.on('disconnect', function(){
+        console.log('Websocket disconnected');
+    });
+
+});
+
+// ZMQ component
 var subscriber = zmq.socket('sub');
 
 var filter = "";
 subscriber.subscribe(filter);
 
-
 subscriber.on('message', function(msg){
     console.log('Received: %s', msg.toString());
+    io.emit('GameState', msg.toString());
 });
 
 console.log('Websocket server trying to connect to ZMQ pub socket on port 3000');
