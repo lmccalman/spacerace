39cba93ca4ef5a3dacf21f7815346752984902af
Brian Thorne <brian.thorne@nicta.com.au>
2015-07-28 11:31:58 +1000

    Add lobby and log socket to frontend server

--- frontend/websocket-server.js
+++ frontend/websocket-server.js
@@ -13,6 +13,7 @@ console.log("Game Server:" + server);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
 // TODO use gulp/webpack etc
 app.use(express.static('node_modules'));
 http.listen(8000, function(){
@@ -35,16 +36,48 @@ io.on('connection', function(ws){
 });
 
 // ZMQ component
-var subscriber = zmq.socket('sub');
+console.log('Frontend is trying to connect to ' + server);
 
-var filter = "";
-subscriber.subscribe(filter);
+var lobbySocket = zmq.socket('req');
+var logSocket = zmq.socket('sub');
+var gameStateSocket = zmq.socket('sub');
 
-subscriber.on('message', function(msg){
+
+//lobbySocket.subscribe("");
+logSocket.subscribe("");
+
+gameStateSocket.subscribe("");
+
+// On receiving a message from the game server
+// Note the `msg` is a string or buffer, not JSON
+lobbySocket.connect('tcp://' + server + ':5558');
+
+lobbySocket.on('message', function(msg){
+    console.log('[LOBBY] Received: %s', msg.toString());
+
+    // Assume game start message?
+    var data = JSON.parse(msg);
+
+    var gameName = data.game;
+    var gameMap = data.map;
+
+    console.log("[LOBBY] Next game " + gameName + " will be on " + gameMap);
+
+});
+
+
+logSocket.on('message', function(msg){
     // Note the `msg` is a string, not JSON
-    //console.log('Received: %s', msg.toString());
+    console.log('[LOG] Received: %s', msg.toString());
+});
+
+gameStateSocket.on('message', function(topic, msg){
+    console.log('[GAME] Received State for: %s', topic.toString());
+    //console.log('[GAME] State: %s', msg.toString());
+
     io.emit('GameState', msg.toString());
 });
 
-console.log('Frontend server is trying to connect to ' + server +' ZMQ pub socket on port 5556');
-subscriber.connect('tcp://' + server + ':5556');
+
+gameStateSocket.connect('tcp://' + server + ':5556');
+logSocket.connect('tcp://' + server + ':5559');
