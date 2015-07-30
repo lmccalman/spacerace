ac13f38cf739183809e55ace6b39ff29052825d9
Louis Tiao <louistiao@gmail.com>
2015-07-30 15:31:45 +1000

    soft tabs

--- clients/python/log_client.py
+++ clients/python/log_client.py
@@ -3,14 +3,14 @@ import zmq
 info_port = 5559
 
 def main():
-	with zmq.Context() as context:
-		with context.socket(zmq.SUB) as socket:
-		    socket.setsockopt_string(zmq.SUBSCRIBE, "")
-		    address = "tcp://localhost:{}".format(info_port)
-		    socket.connect(address)
-		    while True:
-		        msg = socket.recv_json()
-		        print(msg)
+    with zmq.Context() as context:
+        with context.socket(zmq.SUB) as socket:
+            socket.setsockopt_string(zmq.SUBSCRIBE, "")
+            address = "tcp://localhost:{}".format(info_port)
+            socket.connect(address)
+            while True:
+                msg = socket.recv_json()
+                print(msg)
 
 
 if __name__ == '__main__':
