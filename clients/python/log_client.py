e5c8136fc27851db9e80444f84f95f52501a6ce6
Louis Tiao <louistiao@gmail.com>
2015-07-30 15:26:40 +1000

    zmq context and sockets are context managers!

--- clients/python/log_client.py
+++ clients/python/log_client.py
@@ -1,17 +1,16 @@
 import zmq
-import json
 
 info_port = 5559
 
 def main():
-    context = zmq.Context()
-    socket = context.socket(zmq.SUB)
-    socket.setsockopt(zmq.SUBSCRIBE, b"")
-    address = "tcp://localhost:{}".format(info_port)
-    socket.connect(address)
-    while True:
-        msg = json.loads(socket.recv().decode())
-        print(msg)
+	with zmq.Context() as context:
+		with context.socket(zmq.SUB) as socket:
+		    socket.setsockopt_string(zmq.SUBSCRIBE, "")
+		    address = "tcp://localhost:{}".format(info_port)
+		    socket.connect(address)
+		    while True:
+		        msg = socket.recv_json()
+		        print(msg)
 
 
 if __name__ == '__main__':
