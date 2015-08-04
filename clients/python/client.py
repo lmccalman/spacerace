01f3b2d4adab6f21847a1fcd50e9f1a64941909c
Louis Tiao <louistiao@gmail.com>
2015-08-04 18:13:20 +1000

    client

--- clients/python/client.py
+++ clients/python/client.py
@@ -17,9 +17,11 @@ import random
 import json
 import zmq
 
-from zmq.eventloop.ioloop import ZMQIOLoop
-from zmq.eventloop.zmqstream import ZMQStream
-from threading import Thread
+# from zmq.eventloop.ioloop import ZMQIOLoop
+# from zmq.eventloop.zmqstream import ZMQStream
+
+# from threading import Thread
+# from queue import Queue
 
 DEFAULTS = {
     'hostname': 'localhost',
@@ -30,6 +32,7 @@ DEFAULTS = {
 
 # Setup basic logging
 logger = logging.getLogger(__name__)
+
 logging.basicConfig(
     level = logging.DEBUG,
     datefmt = '%H:%M:%S',
@@ -38,12 +41,9 @@ logging.basicConfig(
 
 # Helper functions
 make_address = 'tcp://{}:{}'.format
-
 make_handshake_msg = lambda ship, team: dict(name=ship, team=team)
-
-make_control_str = lambda secret, linear, rotational: ','.join(\
-    [secret, repr(linear), repr(rotational)])
-
+make_control_str = lambda secret, linear, rotational: ','.join \
+    ([secret, repr(linear), repr(rotational)])
 make_random_name = lambda length: ''.join(random.choice(string.ascii_letters) \
     for _ in range(length))
 
@@ -51,6 +51,12 @@ def make_context():
     context = zmq.Context()
     context.linger = 0
     return context
+
+class Bunch(dict):
+
+    def __init__(self,**kw):
+        dict.__init__(self,kw)
+        self.__dict__.update(kw)
 
 class BaseClient:
 
@@ -61,82 +67,89 @@ class BaseClient:
         else:
             self.context = context
 
-        self.addr = make_address(hostname, port)
+        addr = make_address(hostname, port)
+
+        self.sock = self.make_socket()
+        
+        logger.debug('Connecting to "{}"...'.format(addr))
+        self.sock.connect(addr)
+
+    def close(self):
+        self.sock.close()
 
 class LobbyClient(BaseClient):
 
+    def make_socket(self):
+        return self.context.socket(zmq.REQ) 
+
     def register(self, ship_name, team_name):
         
-        with self.context.socket(zmq.REQ) as sock:
-            sock.connect(self.addr)
-            sock.send_json(make_handshake_msg(ship_name, team_name))
-            return sock.recv_json()
+        logger.debug('Registering ship "{}" under team "{}"'.format(ship_name, team_name))
+        self.sock.send_json(make_handshake_msg(ship_name, team_name))
+        
+        logger.debug('Awaiting response from lobby thread...')
+        response = self.sock.recv_json()
+
+        logger.debug('Got "{}"'.format(response))
+        return Bunch(**response)
 
 class ControlClient(BaseClient):
 
-    def send(self, secret_key, linear, rotational):
+    def make_socket(self):
+        return self.context.socket(zmq.PUSH) 
 
-        with self.context.socket(zmq.PUSH) as sock:
-            sock.connect(self.addr)
-            sock.send_string(make_control_str(secret_key, linear, rotational))
+    def send(self, secret_key, linear, rotational):
+        logger.debug('Sending control "{},{},{}"'.format(secret_key, linear, rotational))
+        self.sock.send_string(make_control_str(secret_key, linear, rotational))
 
 class StateClient(BaseClient):
 
-    def subscribe(self, game_name):
+    def __init__(self, game_name, *args, **kwargs):
+        self.game_name = game_name
+        super(StateClient, self).__init__(*args, **kwargs)
 
-        logger.debug(self.addr)
-        self.thread = Thread(name='my_service', target=self.setup, args=(game_name,))
-        self.thread.start()
+    def state_gen(self):
+        while True:
+            state_data = self.recv()
+            if state_data['state'] == 'finished':
+                break
+            yield state_data
 
-    def setup(self, game_name):
+    def make_socket(self):
+        sock = self.context.socket(zmq.SUB)
+        sock.setsockopt_string(zmq.SUBSCRIBE, self.game_name)
+        return sock 
 
-        self.loop = ZMQIOLoop()
-        self.loop.install()
+    def recv(self):
+        msg_filter_b, msg_b = self.sock.recv_multipart()
+        return json.loads(msg_b.decode())
 
-        logger.info('HI!!!')
+# class StateClientIOLoop(BaseClient):
 
-        with self.context.socket(zmq.SUB) as sock:
-            sock.setsockopt_string(zmq.SUBSCRIBE, game_name)
-            sock.connect(self.addr)
+#     def __init__(self, game_name, *args, **kwargs):
+        
+#         self.loop = ZMQIOLoop()
+#         self.loop.install()
 
-            stream = ZMQStream(sock, self.loop)
-            stream.on_recv(self._process_state_msg)            
+#         self.game_name = game_name
 
-            self.loop.start()
+#         super(StateClient, self).__init__(*args, **kwargs)
 
-    def _process_state_msg(self, multipart):
+#     def make_socket(self):
+#         sock = self.context.socket(zmq.SUB)
+#         sock.setsockopt_string(zmq.SUBSCRIBE, self.game_name)
+#         return sock
 
-        msg_filter_b, msg_b = multipart
-        state_data = json.loads(msg_b.decode())
-        if state_data['state'] == 'finished':
-            self.loop.stop()
-        logger.info(pprint.pformat(state_data))
+#     def listen(self):
+#         stream = ZMQStream(self.sock, self.loop)
+#         stream.on_recv(self.process_state)
+#         self.loop.start()
 
-if __name__ == '__main__':
+#     def process_state(self, multipart):
+#         msg_filter_b, msg_b = multipart
+#         state_data = json.loads(msg_b.decode())
+        
+#         if state_data['state'] == 'finished':
+#             self.loop.stop()
 
-    parser = argparse.ArgumentParser(
-        description='Spacerace: Dummy Spacecraft'
-    )
-
-    parser.add_argument('--hostname', type=str, help='Server hostname', default=DEFAULTS['hostname'])
-    parser.add_argument('--state_port', type=int, help='State port', default=DEFAULTS['state_port'])
-    parser.add_argument('--control_port', type=int, help='Control port', default=DEFAULTS['control_port'])
-    parser.add_argument('--lobby_port', type=int, help='Lobby port', default=DEFAULTS['lobby_port'])
-
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-    parser.add_argument('--ship_name', '-n', type=str,
-        default=make_random_name(10), help='Ship Name')
-    parser.add_argument('--team_name', '-t', type=str,
-        default=make_random_name(10), help='Team Name')
-
-    args = parser.parse_args()
-
-    logger.debug(args)
-
-    with make_context() as context:
-        l = LobbyClient(args.hostname, args.lobby_port, context)
-        response = l.register(args.ship_name, args.team_name)
-
-        s = StateClient(args.hostname, args.state_port, context)
-        s.subscribe(response['game'])
-        s.thread.join()
+#         logger.debug([Bunch(**data) for data in state_data['data']])
