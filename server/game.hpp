1ed1287fd08353f0f40c211167e6d699de57a301
Daniel Steinberg <dan.m.steinberg@gmail.com>
2015-08-04 17:27:10 +1000

    logging final game state

--- server/game.hpp
+++ server/game.hpp
@@ -175,7 +175,8 @@ void runGame(PlayerSet& players,
   // Game over, so tell the clients
   playerScore(state, control, map, params, gameStats); // get final score
   logger("game", "status",
-        {{"state","finished"},{"map",map.name}, {"game",gameState.name}}); 
+         {{"state","finished"},{"map",map.name}, {"game",gameState.name},
+          {"ranking", gameStats.playerRanks}}); 
   send(stateSocket, {gameState.name,"{\"state\":\"finished\"}"});
 
 }
