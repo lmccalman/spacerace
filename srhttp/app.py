6918b71f928559f2cef074102601377d11c04c2b
Louis Tiao <louistiao@gmail.com>
2015-11-25 15:25:50 +1100

    lobby

--- srhttp/app.py
+++ srhttp/app.py
@@ -3,9 +3,23 @@ from flask import Flask, jsonify, request
 from string import ascii_letters
 from random import choice
 
+import zmq
+
 app = Flask(__name__)
+app.config.from_object('settings')
 
 make_random_name = lambda length=10: ''.join(choice(ascii_letters) for _ in range(length))
+make_address = 'tcp://{0}:{1}'.format
+
+
+def make_context():
+    context = zmq.Context()
+    context.linger = 0
+    return context
+
+context = make_context()
+lobby_sock = context.socket(zmq.REQ)
+lobby_sock.connect(make_address(app.config.get('SPACERACE_SERVER'), app.config.get('SPACERACE_LOBBY_PORT')))
 
 
 @app.route('/')
@@ -17,7 +31,8 @@ def hello_world():
 def lobby():
     name = request.args.get('name', default=make_random_name())
     team = request.args.get('team', default=make_random_name())
-    return jsonify(msg='Hello welcome to the lobby! Name {0} | Team {1}'.format(name, team))
+    lobby_sock.send_json(dict(name=name, team=team))
+    return jsonify(lobby_sock.recv_json())
 
 
 if __name__ == '__main__':
