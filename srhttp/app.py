f7266105ffe024b3fa440c7694915ff728a09cf1
Louis Tiao <louistiao@gmail.com>
2015-11-25 15:52:59 +1100

    added logging

--- srhttp/app.py
+++ srhttp/app.py
@@ -1,22 +1,13 @@
 from flask import Flask, jsonify, request
-
-from string import ascii_letters
-from random import choice
+from helpers import make_random_name, make_context, make_address
 
 import zmq
 
 app = Flask(__name__)
 app.config.from_object('settings')
 
-make_random_name = lambda length=10: ''.join(choice(ascii_letters) for _ in range(length))
-make_address = 'tcp://{0}:{1}'.format
 
-
-def make_context():
-    context = zmq.Context()
-    context.linger = 0
-    return context
-
+# TODO: context and sockets are never closed
 context = make_context()
 lobby_sock = context.socket(zmq.REQ)
 lobby_sock.connect(make_address(app.config.get('SPACERACE_SERVER'), app.config.get('SPACERACE_LOBBY_PORT')))
@@ -31,9 +22,16 @@ def hello_world():
 def lobby():
     name = request.args.get('name', default=make_random_name())
     team = request.args.get('team', default=make_random_name())
+
+    app.logger.debug('Registering ship "{0}" under team "{1}"'.format(name, team))
     lobby_sock.send_json(dict(name=name, team=team))
-    return jsonify(lobby_sock.recv_json())
+
+    app.logger.debug('Awaiting response from lobby...')
+    response = lobby_sock.recv_json()
+
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    lobby_sock.close()
