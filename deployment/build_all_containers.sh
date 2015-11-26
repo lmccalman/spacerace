be5a78afa681d2b9d7f34ecae6997362505dbb6f
Lachlan McCalman <lachlan.mccalman@nicta.com.au>
2015-11-26 17:12:02 +1100

    update move

--- deployment/build_all_containers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-docker build -t spacerace:base ..
-
-docker build -t spacerace:server server
-docker build -t spacerace:nginx nginx
-docker build -t spacerace:httpserver httpserver 
-docker build -t spacerace:frontend frontend
