5a695c93b63a5a49fb65e925c21d53380a7dab4d
Louis Tiao <louistiao@gmail.com>
2016-05-23 14:17:09 +1000

    removed docker-wait submodule; updated http server to use config

--- httpserver/settings.py
+++ httpserver/settings.py
@@ -1,4 +1,10 @@
-SPACERACE_SERVER = 'server'
-SPACERACE_STATE_PORT = 5556
-SPACERACE_CONTROL_PORT = 5557
-SPACERACE_LOBBY_PORT = 5558
+import os
+import json
+
+with open('/spacerace/config/spacerace.json') as infile:
+    config = json.load(infile)
+
+SPACERACE_SERVER = os.environ.get('SPACERACE_SERVER', 'server')
+SPACERACE_STATE_PORT = config['stateSocket'].get('port', 5556)
+SPACERACE_CONTROL_PORT = config['controlSocket'].get('port', 5557)
+SPACERACE_LOBBY_PORT = config['lobbySocket'].get('port', 5558)
